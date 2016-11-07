234632cc7fa0d7a41459806caec0d9915d75d7d2
Luis Galeas <galeaspablo@hotmail.com>
2016-11-07 09:40:40 +0000

    missing exception tests for FacebookPagesExtension

--- Tests/DependencyInjection/FLFacebookPagesExtensionTest.php
+++ Tests/DependencyInjection/FLFacebookPagesExtensionTest.php
@@ -7,6 +7,7 @@ use FL\FacebookPagesBundle\Model\FacebookUser;
 use FL\FacebookPagesBundle\Model\Page;
 use FL\FacebookPagesBundle\Storage\DoctrineORM\FacebookUserStorage;
 use FL\FacebookPagesBundle\Storage\DoctrineORM\PageStorage;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class FLFacebookPagesExtensionTest extends \PHPUnit_Framework_TestCase
@@ -41,5 +42,51 @@ class FLFacebookPagesExtensionTest extends \PHPUnit_Framework_TestCase
                 'page_class_storage' => PageStorage::class,
             ],
         ], $this->container);
+    }
+
+    /**
+     * @test
+     * @covers \FL\FacebookPagesBundle\DependencyInjection\FLFacebookPagesExtension::load
+     */
+    public function testConfigurationExceptionBadFacebookUserClass()
+    {
+        try {
+            $this->extension->load([
+                'fl_facebook_pages' => [
+                    'facebook_user_class' => Page::class,
+                    'page_class' => Page::class,
+                    'facebook_user_storage' => FacebookUserStorage::class,
+                    'page_class_storage' => PageStorage::class,
+                ],
+            ], $this->container);
+        } catch (InvalidConfigurationException $exception) {
+            $this->assertTrue(true);
+            return;
+        }
+
+        $this->fail(sprintf('Expected %s when loading bad configuration.', InvalidConfigurationException::class));
+    }
+
+    /**
+     * @test
+     * @covers \FL\FacebookPagesBundle\DependencyInjection\FLFacebookPagesExtension::load
+     */
+    public function testConfigurationExceptionBadPageClass()
+    {
+       try {
+           $this->extension->load([
+               'fl_facebook_pages' => [
+                   'facebook_user_class' => FacebookUser::class,
+                   'page_class' => FacebookUser::class,
+                   'facebook_user_storage' => FacebookUserStorage::class,
+                   'page_class_storage' => PageStorage::class,
+               ],
+           ], $this->container);
+       } catch (InvalidConfigurationException $exception) {
+           $this->assertTrue(true);
+           return;
+       }
+
+       $this->fail(sprintf('Expected %s when loading bad configuration.', InvalidConfigurationException::class));
     }
 }
