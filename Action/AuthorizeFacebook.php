84080f448775cd50145516d4863bd7acc9580b15
Luis Galeas <galeaspablo@hotmail.com>
2016-11-08 13:07:33 +0000

    authorize facebook action

---
+++ Action/AuthorizeFacebook.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace FL\FacebookPagesBundle\Action;
+
+use FL\FacebookPagesBundle\Services\Facebook\V2_8\FacebookUserClient;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizeFacebook
+{
+    /**
+     * @var FacebookUserClient
+     */
+    private $facebookUserClient;
+
+    /**
+     * @var string[]
+     */
+    private $permissions;
+
+    /**
+     * @var string
+     */
+    private $callbackUrl;
+
+    /**
+     * @param FacebookUserClient $facebookUserClient
+     * @param string $callBackUrl
+     * @param array $permissions
+     * @link https://developers.facebook.com/docs/facebook-login/permissions
+     */
+    public function __construct(FacebookUserClient $facebookUserClient, string $callBackUrl, array $permissions = ['id'])
+    {
+        $this->facebookUserClient = $facebookUserClient;
+        $this->callbackUrl = $callBackUrl;
+        $this->permissions = $permissions;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request): Response
+    {
+        $helper = $this->facebookUserClient->getRedirectLoginHelper();
+
+        return new RedirectResponse($helper->getLoginUrl($this->callbackUrl, $this->permissions));
+    }
+}
---
+++ Tests/Action/AuthorizeFacebookTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FL\FacebookPagesBundle\Action;
+
+use FL\FacebookPagesBundle\Services\Facebook\V2_8\FacebookUserClient;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class AuthorizeFacebookTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @covers \FL\FacebookPagesBundle\Action\AuthorizeFacebook::__construct
+     * @covers \FL\FacebookPagesBundle\Action\AuthorizeFacebook::__invoke
+     */
+    public function testInvoke()
+    {
+        $facebookUserClient = new FacebookUserClient('fakeAppId', 'fakeAppSecret');
+        $authorizeAction = new AuthorizeFacebook($facebookUserClient, 'https://www.example.com/callbackurl', ['id', 'first_name', 'last_name']);
+
+        /** @var RedirectResponse $response */
+        $response = $authorizeAction(new Request());
+
+        /*
+         * $response->getTargetUrl() will return a url with a variable state query
+         * E.g. ...'state=819273ab81238bcas7123'
+         */
+        $removeStateQueryFromURL = function (string $url) {
+            return preg_replace('/state=.+?(&|$)/', 'state=1234&', $url);
+        };
+
+        $this->assertEquals(
+            $removeStateQueryFromURL($response->getTargetUrl()),
+            'https://www.facebook.com/v2.8/dialog/oauth?client_id=fakeAppId' .
+            '&state=1234'.
+            '&response_type=code&sdk=php-sdk-5.4.0&redirect_uri='.
+            'https%3A%2F%2Fwww.example.com%2Fcallbackurl&scope=id%2Cfirst_name%2Clast_name');
+    }
+}
